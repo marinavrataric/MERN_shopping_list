9a96e51b76c6137cf6941b3b9638d150dd26644b
marina <marina.vrataric@gmail.com>
2020-05-12 17:19:56 +0200

    mongoDB connected

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://marina123:marina123@tmcluster-0sn6x.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
+
+// MongoDB config
+const db = require('./config/keys').mongoURI
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
